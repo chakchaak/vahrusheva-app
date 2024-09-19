781b7455643c73f009300e4cedcf31558166d971
chakchaak <dariavah8106@gmail.com>
2024-09-19 13:02:12 +0500

    create project

---
+++ resources/views/first.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Первая страница</h1>
+    <p>{{ $sum }}</p>
+    <p>{{ $a }}</p>
+    <p>{{ $b }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aspernatur iusto deserunt? Veritatis, omnis officia sed aut impedit, quaerat beatae a nostrum soluta unde magni expedita enim! Assumenda, praesentium mollitia.</p>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Главная страница</h1>
+    <a href="{{ route('first') }}">На первую страницу</a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorum assumenda, 
+        odio vel architecto nesciunt esse id obcaecati adipisci, 
+        pariatur sunt recusandae placeat. Minus laudantium suscipit libero veniam officia impedit!</p>
+</body>
+</html>
